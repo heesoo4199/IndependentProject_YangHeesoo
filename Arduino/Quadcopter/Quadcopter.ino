3f7602697fe365e1f399e3aaea33cc1258b272d9
Heesoo Mac <heesoo4199@gmail.com>
2017-10-25 10:00:04 -0700

    Commit 10/25
    MPU-6050 initialization implemented -> turn sleep mode off, set sensitivities for gyro and accelerometer, and started working on deriving values from the sensor.

--- Arduino/Quadcopter/Quadcopter.ino
+++ Arduino/Quadcopter/Quadcopter.ino
@@ -1,13 +1,49 @@
 #include <Wire.h>
 
+float rotX, rotY, rotZ;
+float accX, accY, accZ;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Wire.begin();
-  Wire.beginTransmission(0b1101000);
+  mpuInit(); // Initializes the MPU
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+// Disables sleep mode, and sets sensitivity for the gyro and accelerometer.
+// https://www.invensense.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
+void mpuInit() {
+  // Disable sleep mode
+  Wire.beginTransmission(0b1101000); // MPU access register
+  Wire.write(0x6B); // 6B register for power management
+  Wire.write(0b00000000); // 2nd bit = 0 for sleep off
+  Wire.endTransmission();
+  // Set gyro sensitivity
+  Wire.beginTransmission(0b1101000);
+  Wire.write(0x1B);
+  Wire.write(0b00000000);
+  Wire.endTransmission();
+  // Set accelerometer sensitivity
+  Wire.beginTransmission(0b1101000);
+  Wire.write(0x1C);
+  Wire.write(0b00000000);
+  Wire.endTransmission();
+}
+
+// Sets rotX, rotY, and rotZ to respective gyro values (in degrees per second)
+void gyroRead() {
+  Wire.beginTransmission(0b11010001);
+  //Wire.write(
+}
+
+// Sets accX, accY, and accZ to respective accelerometer values (in Gs)
+void accelRead() {
+
+  
+}
+
