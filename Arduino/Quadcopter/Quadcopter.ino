10dcac64d555dfcb798c51bf9b784210424f9bec
Heesoo Mac <heesoo4199@gmail.com>
2017-10-30 09:53:49 -0700

    Commit 10/27
    I thought I had commited AND pushed to master, but I didn't check and it looks like it was not actually commited. On 10/27, I worked on finishing up the I2C transmission from the gyro/accel to the arduino. In the new code, I set the sensitivities of the gyro to 131 Least Significant Bits per degree per second, and the accelerometer to 16384 LSB/g. These should be the most sensitive settings, if my understanding of LSBs are remotely correct. Afterwards, I request values from the gyro/accel in the main update loop and attempt to print to the serial(console), but for some reason, nothing is printing to the serial.

--- Arduino/Quadcopter/Quadcopter.ino
+++ Arduino/Quadcopter/Quadcopter.ino
@@ -2,17 +2,26 @@
 
 float rotX, rotY, rotZ;
 float accX, accY, accZ;
+float angleX, angleY, angleZ;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Wire.begin();
   mpuInit(); // Initializes the MPU
+  angleX = 0;
+  angleY = 0;
+  angleZ = 0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  gyroRead();
+  angleX += rotX;
+  angleY += rotY;
+  angleZ += rotZ;
+  Serial.print(angleX);
+  delay(100);
 }
 
 // Disables sleep mode, and sets sensitivity for the gyro and accelerometer.
@@ -37,13 +46,28 @@ void mpuInit() {
 
 // Sets rotX, rotY, and rotZ to respective gyro values (in degrees per second)
 void gyroRead() {
-  Wire.beginTransmission(0b11010001);
-  //Wire.write(
+  Wire.beginTransmission(0b11010000);
+  Wire.write(0x43);
+  Wire.endTransmission();
+  Wire.requestFrom(0b11010000, 6); // Requests values from address 43 to 48
+  while (Wire.available() < 6) { 
+    rotX = (Wire.read()<<8|Wire.read()) / 131.0; // Divid raw value by 131 (LSB per degree)
+    rotY = (Wire.read()<<8|Wire.read()) / 131.0;
+    rotZ = (Wire.read()<<8|Wire.read()) / 131.0;
+  }
 }
 
 // Sets accX, accY, and accZ to respective accelerometer values (in Gs)
 void accelRead() {
-
+  Wire.beginTransmission(0b11010000);
+  Wire.write(0x3B);
+  Wire.endTransmission();
+  Wire.requestFrom(0b11010000, 6); // Requests values from address 3B to 40
+  while (Wire.available() < 6) { 
+    accX = (Wire.read()<<8|Wire.read()) / 16384.0; // Divid raw value by 16384 (LSB per g)
+    accY = (Wire.read()<<8|Wire.read()) / 16384.0;
+    accZ = (Wire.read()<<8|Wire.read()) / 16384.0;
+  }
   
 }
 
