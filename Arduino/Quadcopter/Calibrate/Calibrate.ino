5fb037501c6f8fe3210b95e95aef1056dcade776
Heesoo Mac <heesoo4199@gmail.com>
2017-11-14 10:20:08 -0800

    Commit 11/14
    Did work mostly on hardware, tried calibrating the ESC but my calibration code wasn't working or my potentiometer wasn't working. I will have to try with a new potentiometer at home and see whether it is the problem with the ESC's calibration program or my arduino program. Once ESCs are calibrated, I can move on with finishing PID yay.

--- Arduino/Quadcopter/Calibrate/Calibrate.ino
+++ Arduino/Quadcopter/Calibrate/Calibrate.ino
@@ -16,4 +16,4 @@ void loop()
   val = map(val, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 0 and 180) 
   ESC.write(val);                  // sets the servo position according to the scaled value 
   delay(10); // 100Hz, does it work?
-} 
+} //
