b7faa8564183edd4ed1a6eb666454f4d74ff1a63
heesoo4199 <heesoo4199@users.noreply.github.com>
2017-11-22 08:14:11 -0800

    Updated Calibrate.ino
    Better code, and can be used to test functionality for all 4 motors simultaneously using analog input from 1000us to 2000us

--- Arduino/Quadcopter/Calibrate/Calibrate.ino
+++ Arduino/Quadcopter/Calibrate/Calibrate.ino
@@ -1,19 +1,30 @@
-#include <Servo.h> 
+#include <Servo.h>//Using servo library to control ESC
+Servo esc1; //Creating a servo class with name as esc
+Servo esc2; //Creating a servo class with name as esc
+Servo esc3; //Creating a servo class with name as esc
+Servo esc4; //Creating a servo class with name as esc
+void setup()
+{
+  pinMode(1, OUTPUT);
+esc1.attach(7); //Specify the esc signal pin,Here as D8
+esc1.writeMicroseconds(1000); //initialize the signal to 1000
+esc2.attach(2); //Specify the esc signal pin,Here as D8
+esc2.writeMicroseconds(1000); //initialize the signal to 1000
+esc3.attach(3); //Specify the esc signal pin,Here as D8
+esc3.writeMicroseconds(1000); //initialize the signal to 1000
+esc4.attach(4); //Specify the esc signal pin,Here as D8
+esc4.writeMicroseconds(1000); //initialize the signal to 1000
 
-Servo ESC;
-
-int pin = 0;  // potentiometer pin no.
-int val;    // variable to read the value from the analog pin 
-
-void setup() 
-{ 
-  ESC.attach(9);  // Use digital
-} 
-
-void loop() 
-{ 
-  val = analogRead(pin);            // reads the value of the potentiometer (value between 0 and 1023) 
-  val = map(val, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 0 and 180) 
-  ESC.write(val);                  // sets the servo position according to the scaled value 
-  delay(10); // 100Hz, does it work?
-} //
+Serial.begin(9600);
+}
+void loop()
+{
+int val; //Creating a variable val
+val= analogRead(A0); //Read input from analog pin a0 and store in val
+val= map(val, 0, 1023,1000,2000); //mapping val to minimum and maximum(Change if needed) 
+esc1.writeMicroseconds(val); //using val as the signal to esc
+esc2.writeMicroseconds(val);
+esc3.writeMicroseconds(val);
+esc4.writeMicroseconds(val);
+Serial.println(val);
+}
