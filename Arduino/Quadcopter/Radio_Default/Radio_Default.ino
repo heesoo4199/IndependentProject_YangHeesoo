12fe24c3b8430b8296d0effbf898110ed1d33ded
heesoo4199 <heesoo4199@users.noreply.github.com>
2018-01-26 08:06:34 -0800

    Radio transmitter code

--- Arduino/Quadcopter/Radio_Default/Radio_Default.ino
+++ Arduino/Quadcopter/Radio_Default/Radio_Default.ino
@@ -1,103 +1,24 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
- 
-// SOURCE CODE (RADIO INITIALIZATION): HOWTOMECHATRONICS.COM AND ADAPTED BY NATHAN SHAO AND HEESOO YANG
-RF24 radio(7, 8);
-const byte designation[6] = "00001";
-int radioNum = 1;  /* 0 for master, 1 for slave */
- 
-void setup()
-{
+
+RF24 radio(7,8);
+
+const byte address[6] = "00001";
+
+void setup() {
   radio.begin();
- 
-  if(radioNum == 0)
-  {
-    Serial.begin(9600);
-    radio.openWritingPipe(designation);
-    radio.setPALevel(RF24_PA_MIN);
-    radio.stopListening();
-  }
-  else
-  {
-    Serial.begin(9600);
-    radio.openReadingPipe(0, designation);
-    radio.setPALevel(RF24_PA_MIN);
-    radio.startListening();
-  }
- 
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_250KBPS);
+  radio.stopListening();
 }
- 
- 
-void loop()
-{
- 
-  if(radioNum == 0)
-  {
-    const char message[] = "Testing";
-   
-    /*if (Serial.available())
-      {
-        strcpy(message, Serial.read());
-      }
-      */
-     
-    if (!radio.write(&message,strlen(message)))
-     {
-       Serial.println(F("failed"));
-     }
-    
-    Serial.println(message);
- 
-    delay(100);
-  }
-  else
-  {
-    String altitude = "";
-    String X_Tilt = "";
-    String Y_Tilt = "";
-    String Clockwise_Yaw = "";
-    String Counter_Yaw = "";
-   
-    if (radio.available())
-    {
-      char message[32] = "";
-      radio.read(&message, sizeof(message));
-      Serial.println(message);
- 
-      /*
-      for(int i = 0; i < 4; i++)
-      {
-       altitude += message[i];
-        X_Tilt += message[i+4];
-        Y_Tilt += message[i+8];
-        Clockwise_Yaw += message[i+12];
-        Counter_Yaw += message[i+16];
-      }
-     
-      if (!altitude.substring(0,2).equals("XX"))
-      {
-        Serial.print("altitude, ");
-      }
-      if (!X_Tilt.substring(0,2).equals("XX"))
-      {
-        Serial.print("X, ");
-      }
-      if (!Y_Tilt.substring(0,2).equals("XX"))
-      {
-        Serial.print("Y, ");
-      }
-      if (!Clockwise_Yaw.substring(0,2).equals("XX"))
-      {
-        Serial.print("Clockwise, ");
-      }
-      if (!Counter_Yaw.substring(0,2).equals("XX"))
-      {
-        Serial.print("Counter-Clockwise");
-      }
-      */
-    }
-  }
- 
- 
+
+void loop() {
+  String str = "";
+  str += analogRead(0);
+  int str_len = str.length() + 1;
+  char text[str_len];
+  str.toCharArray(text, str_len);
+  radio.write(&text, sizeof(text));
 }
