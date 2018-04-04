e8141d189f640e227f1a305aaf0215a6eec94d31
Heesoo Mac <heesoo4199@gmail.com>
2018-04-04 09:24:29 -0700

    Commit 4/4
    Set up the project, imported necessary imports (notably GameStrap UI manager, the asset-store plugin that makes creating a good looking UI extremely easy and comes with its own set of assets), and started working on the main game mechanic. I will focus on the main game mechanic (creating the grid, making the pieces drop and fit-in together...), then I will work on user input such as dropping, rotating, holding, showing upcoming pieces, showing where they piece would land, etc.

---
+++ Unity Tetris/Assets/Scenes/Title/TitleSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartPressed() {
+		SceneManager.LoadScene("Main"); // SceneManager.LoadScene("OtherSceneName", LoadSceneMode.Additive);
+	}
+
+}
