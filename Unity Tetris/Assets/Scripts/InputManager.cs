8f94afbe8da05f6434b7d5f56dfcea3d0778ebaf
Heesoo Mac <heesoo4199@gmail.com>
2018-04-06 10:20:27 -0700

    Commit 4/5
    Further worked on setting up the project and the UI (though it's not visible yet). I also worked on doing some player input handling, but I spent time on paper trying to think of how to implement the grid that would make organization not a complete mess.

---
+++ Unity Tetris/Assets/Scripts/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			Rotate ();
+		}	
+	}
+
+	void Rotate() {
+	}
+
+	void Drop() {
+	}
+
+	void Hold() {
+
+	}
+
+	void Accelerate() {
+
+	}
+}
